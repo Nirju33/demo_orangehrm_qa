91e395004d57357413b388b0420882ff65bc53b9
Nirjala K.C. <kcnirjala88@gmail.com>
2026-07-10 12:44:51 +0545

    feat: Add login page and test

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,15 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
     URL = "https://opensource-demo.orangehrmlive.com/"
     
-    # --- Login Page Locators ---
+    # --- Locators ---
     USERNAME_INPUT = (By.NAME, "username")
     PASSWORD_INPUT = (By.NAME, "password")
     LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
-    FORGOT_PASSWORD_LINK = (By.XPATH, "//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")
-    
-    # --- Reset Password Page Locators ---
-    RESET_USERNAME_INPUT = (By.NAME, "username")
-    RESET_BUTTON = (By.XPATH, "//button[contains(@class, 'orangehrm-forgot-password-button--reset')]") 
-
+  
+ 
     def load(self):
         self.open(self.URL)
         self._wait_for_page_ready()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import time
 from pages.login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,4 +14,37 @@ def test_login_with_invalid_credentials(driver):
     loginpage.load()
     loginpage.login("Admin", "admin1234")
     assert "dashboard" not in driver.current_url
-   
+
+def test_Login_with_empty_credentials(driver):
+    loginpage = LoginPage(driver)
+    loginpage.load()
+    loginpage.login("", "")
+    assert "dashboard" not in driver.current_url
+
+def test_login_with_blank_username(driver):
+    loginpage = LoginPage(driver)
+    loginpage.load()
+    loginpage.login("", "admin123")
+    assert "dashboard" not in driver.current_url
+
+def test_login_with_blank_password(driver):
+    loginpage = LoginPage(driver)
+    loginpage.load()
+    loginpage.login("Admin", "")
+    assert "dashboard" not in driver.current_url
+
+def test_login_with_special_characters(driver):
+    loginpage = LoginPage(driver)
+    loginpage.load()
+    loginpage.login("Admin!@#", "admin123")
+    assert "dashboard" not in driver.current_url
+
+def test_login_with_long_username(driver):
+    loginpage = LoginPage(driver)
+    loginpage.load()
+    long_username = "A" * 256  # 256 characters long
+    loginpage.login(long_username, "admin123")
+    assert "dashboard" not in driver.current_url
+
+
+    time.sleep(10)
