0b42d1eb6bbf7e853b728c53e3259b9d38dbc0e5
Nirjala K.C. <kcnirjala88@gmail.com>
2026-07-17 23:21:02 +0545

    feat: Add admin page and test

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -16,6 +16,7 @@ class AdminPage(BasePage):
     ADMIN_MENU_BUTTON = (By.XPATH, "//a[span[text()='Admin']]")
     ADD_BUTTON = (By.XPATH, "//button[normalize-space()='Add']")
     SAVE_BUTTON = (By.XPATH, "//button[@type='submit' and contains(., 'Save')]")
+    SPINNER = (By.CLASS_NAME, "oxd-loading-spinner")
     
     # ==========================================
     # USER MANAGEMENT LOCATORS
@@ -25,6 +26,7 @@ class AdminPage(BasePage):
     RECORDS_FOUND_LABEL = (By.XPATH, "//span[contains(., 'Found')]")
     FIRST_ROW_USERNAME_CELL = (By.XPATH, "//div[@class='oxd-table-body']/div[1]/div/div[2]/div")
     
+    # User Creation
     USER_ROLE_DROPDOWN = (By.XPATH, "//div[label[text()='User Role']]/following-sibling::div//div[@class='oxd-select-text-input']")
     EMPLOYEE_NAME_INPUT = (By.XPATH, "//div[label[text()='Employee Name']]/following-sibling::div//input")
     STATUS_DROPDOWN = (By.XPATH, "//div[label[text()='Status']]/following-sibling::div//div[@class='oxd-select-text-input']")
@@ -46,24 +48,33 @@ class AdminPage(BasePage):
     
     PAY_GRADE_NAME_INPUT = (By.XPATH, "//div[label[text()='Name']]/following-sibling::div/input")
     
-    # CURRENCY BOX LOCATORS
-    CURRENCY_SECTION_ADD_BUTTON = (By.XPATH, "//h6[text()='Currencies']/following-sibling::button[contains(., 'Add')]")
-    CURRENCY_DROPDOWN = (By.XPATH, "//div[label[text()='Currency']]/following-sibling::div//div[@class='oxd-select-text-input']")
-    MIN_SALARY_INPUT = (By.XPATH, "//div[label[text()='Minimum Salary']]/following-sibling::div/input")
-    MAX_SALARY_INPUT = (By.XPATH, "//div[label[text()='Maximum Salary']]/following-sibling::div/input")
-    CURRENCY_SAVE_BUTTON = (By.XPATH, "//h6[text()='Add Currency']/following-sibling::form//button[@type='submit' and contains(., 'Save')]")
-    # METHODS
-    # ==========================================
+    # CURRENCY BOX LOCATORS (प्रस्ट र बलियो बनाइएको XPATH)
+    CURRENCY_SECTION_ADD_BUTTON = (By.XPATH, "//h6[text()='Currencies']/following-sibling::button | //h6[text()='Currencies']/..//button[contains(., 'Add')]")
+    CURRENCY_DROPDOWN = (By.XPATH, "//label[text()='Currency']/../following-sibling::div//div[@class='oxd-select-text-input']")
+    MIN_SALARY_INPUT = (By.XPATH, "//label[text()='Minimum Salary']/../following-sibling::div/input")
+    MAX_SALARY_INPUT = (By.XPATH, "//label[text()='Maximum Salary']/../following-sibling::div/input")
+    CURRENCY_SAVE_BUTTON = (By.XPATH, "//h6[text()='Add Currency']/following-sibling::form//button[@type='submit']")
+
+    def wait_for_spinner_to_disappear(self):
+        try:
+            WebDriverWait(self.driver, 3).until(EC.presence_of_element_located(self.SPINNER))
+            WebDriverWait(self.driver, 15).until(EC.invisibility_of_element_located(self.SPINNER))
+        except:
+            pass
+
     def navigate_to_admin_panel(self):
         self.click(self.ADMIN_MENU_BUTTON)
         self.wait_for_url_contains("admin/viewSystemUsers")
+        self.wait_for_spinner_to_disappear()
 
     def click_add(self):
         self.click(self.ADD_BUTTON)
+        self.wait_for_spinner_to_disappear()
 
     def save_form(self):
         self.click(self.SAVE_BUTTON)
-        time.sleep(2) 
+        self.wait_for_spinner_to_disappear()
+        time.sleep(1) 
         
     def create_new_user(self, role, employee_name, username, password, status="Enabled"):
         self.click(self.USER_ROLE_DROPDOWN)
@@ -107,7 +118,7 @@ class AdminPage(BasePage):
         
         self.click(self.SEARCH_BUTTON)
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.RECORDS_FOUND_LABEL))
-        time.sleep(1)
+        self.wait_for_spinner_to_disappear()
 
     def get_records_text(self):
         return self.find_visible(self.RECORDS_FOUND_LABEL).text
@@ -119,6 +130,7 @@ class AdminPage(BasePage):
         self.click(self.JOB_MENU)
         self.click(self.JOB_TITLES_OPTION)
         self.wait_for_url_contains("admin/viewJobTitleList")
+        self.wait_for_spinner_to_disappear()
     
     def create_new_job_title(self, title, description, file_path, note):
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.JOB_TITLE_INPUT)).send_keys(title)
@@ -128,79 +140,48 @@ class AdminPage(BasePage):
 
         save_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.SAVE_BUTTON))
         save_btn.click()
+        self.wait_for_spinner_to_disappear()
     
     def navigate_to_pay_grades(self):
         self.click(self.JOB_MENU)
         self.click(self.PAY_GRADES_OPTION)
         self.wait_for_url_contains("admin/viewPayGrades")
+        self.wait_for_spinner_to_disappear()
 
     def create_pay_grade_name(self, grade_name):
-        """सुरुमा Pay Grade को नाम मात्र राखेर Save गर्ने"""
         el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.PAY_GRADE_NAME_INPUT))
         el.send_keys(grade_name)
         
         first_save_btn = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
+            EC.element_to_be_clickable(self.SAVE_BUTTON)
         )
         first_save_btn.click()
-        time.sleep(3)
+        self.wait_for_spinner_to_disappear()
         
     def add_currency_to_grade(self, currency_name, min_salary, max_salary):
-        """Currencies को + Add बटन थिचेर Topbar ले नछोपिने गरी विवरण भर्ने र Save गर्ने"""
-        # १. पेज लोड हुन निश्चित गर्ने
-        WebDriverWait(self.driver, 20).until(
-            EC.url_contains("payGrade")
+        currency_add_btn = WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable(self.CURRENCY_SECTION_ADD_BUTTON)
         )
-        time.sleep(2)
-
-        # २. 'Currencies' सेक्सनको '+ Add' बटन खोज्ने र स्क्रोल गरेर क्लिक गर्ने
-        add_currency_btn = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located(self.CURRENCY_SECTION_ADD_BUTTON)
+        currency_add_btn.click()
+        self.wait_for_spinner_to_disappear()
+        dropdown_trigger = WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable(self.CURRENCY_DROPDOWN)
         )
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", add_currency_btn)
-        time.sleep(0.5)
-        
-        try:
-            add_currency_btn.click()
-        except Exception:
-            self.driver.execute_script("arguments[0].click();", add_currency_btn)
-            
-        # फारम विस्तार (open) हुन थोरै समय पर्खिने
-        time.sleep(1.5)
-
-        # ३. Currency ड्रपडाउन खोज्ने
-        dropdown = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located(self.CURRENCY_DROPDOWN)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", dropdown)
-        time.sleep(0.5)
-        self.driver.execute_script("arguments[0].click();", dropdown)
-        time.sleep(1.5)
-        
-        # ४. लिस्टबाट सही Currency छान्ने
+        dropdown_trigger.click()
         currency_option = WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.XPATH, f"//div[@role='listbox']//*[contains(text(), '{currency_name}')]"))
         )
         currency_option.click()
-        time.sleep(1)
-        
-        # ५. Minimum Salary भर्ने
-        min_el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.MIN_SALARY_INPUT))
-        min_el.click()
-        min_el.send_keys(Keys.CONTROL + "a" + Keys.BACKSPACE)
-        min_el.send_keys(min_salary)
-        
-        # ६. Maximum Salary भर्ने
-        max_el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.MAX_SALARY_INPUT))
-        max_el.click()
-        max_el.send_keys(Keys.CONTROL + "a" + Keys.BACKSPACE)
-        max_el.send_keys(max_salary)
-        
-        # ७. मुद्रा फारमको Save बटन खोज्ने र JS Click मार्फत क्लिक गर्ने
-        save_btn = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located(self.CURRENCY_SAVE_BUTTON)
+        min_sal = WebDriverWait(self.driver, 15).until(
+            EC.visibility_of_element_located(self.MIN_SALARY_INPUT)
         )
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", save_btn)
-        time.sleep(0.5)
-        self.driver.execute_script("arguments[0].click();", save_btn)
-        time.sleep(3)
+        min_sal.send_keys(min_salary)
+        max_sal = WebDriverWait(self.driver, 15).until(
+            EC.visibility_of_element_located(self.MAX_SALARY_INPUT)
+        )
+        max_sal.send_keys(max_salary)
+        save_button = WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable(self.CURRENCY_SAVE_BUTTON)
+        )
+        save_button.click()
+        self.wait_for_spinner_to_disappear()
